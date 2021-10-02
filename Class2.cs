2637ec3c434aeb9244dc386740717ebd4b5135ef
Deepana Balakrishnan <deepanabala@gmail.com>
2021-10-02 12:20:53 +0530

    All new changes

--- Class2.cs
+++ Class2.cs
@@ -5,8 +5,19 @@ using System.Web;
 
 namespace SimpleCSharpwebformappln
 {
-    public partial class ClassA
+    public partial class ClassA 
+        //: IProduct,ICustomer
     {
-        public int u = 9;
+        readonly int year;
+        const int n = 6;
+        //public int u = 9;
+        // string IProduct.GetSoldItem()
+        //{
+        //    return "A";
+        //}
+        // string ICustomer.GetSoldItem()
+        //{
+        //    return "F   ";
+        //}
     }
 }
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -17,7 +17,7 @@ namespace SimpleCSharpwebformappln
             List<string> ts = new List<string> { "Go", "fgf", "ind" };
             List<int> i = new List<int> { 1, 1, 4, 5, 6, 4 };
             int dup = 1;
-
+            
             List<ClassA> CustomerData = new List<ClassA>() { new ClassA() { StudentID = 1, StudentName = "123" } };
             string Convertsd = Convert.ToString(dup);
             string Converts = dup.ToString();
@@ -33,14 +33,14 @@ namespace SimpleCSharpwebformappln
             }
             //partial class
             ClassA obj = new ClassA();
-       // int g = obj.a;
-        string a = ts.Contains("Go").ToString();
-        List<int> Top5 = new List<int>(i.Where(p => p > 5).Take(5).ToList());
+            // int g = obj.a;
+            string a = ts.Contains("Go").ToString();
+            List<int> Top5 = new List<int>(i.Where(p => p > 5).Take(5).ToList());
             //Select
-           // List<int> Select5 =  i.Select(x => x == 8);
-        //IEnumerable<DataRow> = i.DataTable.AsEnumerable()
-        //First
-        int First = i.First(x => i.Contains(1));
+            // List<int> Select5 =  i.Select(x => x == 8);
+            //IEnumerable<DataRow> = i.DataTable.AsEnumerable()
+            //First
+            int First = i.First(x => i.Contains(1));
             //throwing error since 0 is not in the list
             //int Firstnotinlist = i.First(x => i.Contains(0));
 
@@ -48,11 +48,47 @@ namespace SimpleCSharpwebformappln
             //Orderby
             List<ClassA> Orerby = CustomerData.OrderBy(x => x.StudentID).ToList();
             //
-            int FirstorDefault =i.FirstOrDefault(x=>x == 2);
+            int FirstorDefault = i.FirstOrDefault(x => x == 2);
 
-            IEnumerable<ClassA> testienum = CustomerData.Where(p => p.StudentID == 1); 
+            IEnumerable<ClassA> testienum = CustomerData.Where(p => p.StudentID == 1);
 
+            /*
+             * 
+             * 
+             * 
+             * */
+            string Sentencewithdates = "The UN was established on 24-10-1945";
+            string newstring = "";
+            for (int ia = 0; ia < Sentencewithdates.Length; ia++)
+            { 
+                if (char.IsDigit(Sentencewithdates[ia]))
+                {
+                    newstring +=  Sentencewithdates[ia];
+                }
+                // if we found - then clear the str2
+                if (Sentencewithdates[ia] == '-')
+                {
+                    newstring = "";
+                }
 
+            }
+            if (newstring.Length > 1)
+            {
+                for (int dis = 0; dis <= newstring.Length; dis++)
+                {
+                    if (newstring.Length == 4)
+                    {
+                        string year = newstring;
+                        Response.Write(year);
+                    }
+                    //if(newstring.Contains("-"))
+                    //    {
+                    //        newstring.Replace("-", " ");
+                    //    }
+                    //}
+
+                }
+            }
         }
     }
 }
