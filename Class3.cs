549e4a6ef2259bc60a9f0aad483e1ceddfb5daad
Deepana Balakrishnan <deepanabala@gmail.com>
2021-10-03 15:53:01 +0530

    hyuiy

--- Class3.cs
+++ Class3.cs
@@ -5,13 +5,14 @@ using System.Web;
 
 namespace SimpleCSharpwebformappln
 {
-    public class Class3 
+    public class Class3 : SealedClass
     {
         public readonly int year =8;
         public const int n = 6;
         public int b = 9;
         public Class3()
         {
+            year = g;
             year = 9;
         }
         
